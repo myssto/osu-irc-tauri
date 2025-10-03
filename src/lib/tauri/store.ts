d99a118aa83d3859c02d3c8c2c9f96260e19e502
myssto <pat.lacni@yahoo.com>
2025-10-03 02:18:05 -0500

    wip: irc connection

--- src/lib/tauri/store.ts
+++ src/lib/tauri/store.ts
@@ -1,37 +1,41 @@
 import { load, type Store } from "@tauri-apps/plugin-store";
 
-export interface IStoreData extends Record<string, unknown> {
+export interface IStoreData {
   ircUsername?: string;
   ircPassword?: string;
   saveIrcCredentials: boolean;
 }
 
-const storeDefaults: IStoreData = {
+const storeDefaults: IStoreData & { [key: string]: unknown } = {
   saveIrcCredentials: true,
 };
 
 export interface IAppStore {
-  rawStore: Store;
+  raw: Store;
 
-  get: <K extends (keyof IStoreData)[]>(
-    ...keys: K
-  ) => Promise<{
-    [P in K[number]]: IStoreData[P];
-  }>;
+  get: <K extends keyof IStoreData>(
+    key: K
+  ) => Promise<IStoreData[K] | undefined>;
 
-  set: <K extends Extract<keyof IStoreData, string>>(
+  set: <K extends keyof IStoreData>(
     key: K,
     value: IStoreData[K]
   ) => Promise<void>;
-
-  has: (key: Extract<keyof IStoreData, string>) => Promise<boolean>;
-
-  delete: (key: Extract<keyof IStoreData, string>) => Promise<boolean>;
 }
 
 export const store = async (): Promise<IAppStore> => {
-  const rawStore = await load("store.json", {
+  const raw = await load("store.json", {
     defaults: storeDefaults,
     autoSave: false,
   });
+
+  return {
+    raw,
+    get: async (key) => {
+      return await raw.get(key as string);
+    },
+    set: async (key, value) => {
+      return await raw.set(key as string, value);
+    },
+  };
 };
